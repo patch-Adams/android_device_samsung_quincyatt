a143e96765dc7d3eaf976533baa597925e095b46
patch_Adams <walpat11@gmail.com>
2013-12-08 08:12:47 -0600

    liquify

--- liquid.mk
+++ liquid.mk
@@ -1,15 +1,14 @@
 $(call inherit-product, device/samsung/quincyatt/full_quincyatt.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
+# Inherit some common liquid stuff.
+$(call inherit-product, vendor/liquid/config/common_gsm.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common liquid stuff.
+$(call inherit-product, vendor/liquid/config/common_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I717 TARGET_DEVICE=SGH-I717 BUILD_FINGERPRINT="samsung/SGH-I717/SGH-I717:4.4/KRT16M/I717UCMD3:user/release-keys" PRIVATE_BUILD_DESC="SGH-I717-user 4.4 KRT16M I717UCMD3 release-keys"
 
-PRODUCT_NAME := cm_quincyatt
+TARGET_BOOTANIMATION_NAME := vertical-800x1280
+
+PRODUCT_NAME := liquid_quincyatt
 PRODUCT_DEVICE := quincyatt
